27c774aeacb918558cf3d373e4437c59963f332b
qiuxan <qiuxan@gmail.com>
2023-07-03 19:11:33 +1000

    use children for text

--- src/App.tsx
+++ src/App.tsx
@@ -86,7 +86,15 @@ function App() {
 
   return (
     <div>
-      <ExpandableText cutToNumber={30} />
+      <ExpandableText cutToNumber={30}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </ExpandableText>
       <div>
         <h3>Cart</h3>
         <ul>

b1c5edf8f425e28e69b10b487a435dc6a1aeb68b
qiuxan <qiuxan@gmail.com>
2023-07-08 14:00:35 +1000

    007-17- Creating a Custom Data Fetching Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
 import { useEffect, useState } from "react";
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/user-service";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setIsLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
-
+  const { users, error, isLoading, setUsers, setError } = useUsers();
   const deleteUser = (user: User) => {
     const originalUser = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
---
+++ src/hooks/useUsers.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { CanceledError } from "../services/api-client";
+import userService, { User } from "../services/user-service";
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const { request, cancel } = userService.getAll<User>();
+    request
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setIsLoading(false);
+      });
+
+    return () => cancel();
+  }, []);
+  return { users, error, isLoading, setUsers, setError };
+};
+
+export default useUsers;

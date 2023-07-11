193decc5b08854ea28edbbf95cb90fd06dca6836
qiuxan <qiuxan@gmail.com>
2023-07-11 21:25:01 +1000

    008-19- Displaying the Genres

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -9,12 +9,18 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GenreList />
+        <GridItem area="aside" paddingX={5}>
+          <GenreList />
+        </GridItem>
       </Show>
 
       <GridItem area="main">
--- game-hub/src/component/GenreList.tsx
+++ game-hub/src/component/GenreList.tsx
@@ -1,13 +1,24 @@
 import useGenres from "../hooks/useGenres";
+import { Image, List, ListItem, HStack, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              src={getCroppedImageUrl(genre.image_background)}
+              borderRadius={10}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 

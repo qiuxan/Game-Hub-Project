4edd388223ed12f7271fa122bf840dfd440b0575
qiuxan <qiuxan@gmail.com>
2023-08-01 09:30:30 +1000

    setup gener using RQ, check the change of types

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClients from "../services/api-clients";
 import useData from "./useData";
+import { FetchResponse } from "./useData";
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClients.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+  });
+// useData<Genre>("/genres");
 
 export default useGenres;

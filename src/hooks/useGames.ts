a04975e456d38ce8cdd53330731f4fe3fd52655a
qiuxan <qiuxan@gmail.com>
2023-08-09 21:21:18 +1000

    004-16- Exercise- Refactoring Entities

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,23 +1,9 @@
 import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-clients";
 import { FetchResponse } from "../services/api-clients";
-import { Genre } from "./useGenres";
-import { Platform } from "./usePlatforms";
 import ms from "ms";
 import useGameQueryStore from "../store";
-
-export interface Game {
-  background_image: string;
-  slug: string;
-  id: number;
-  name: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  genres: Genre[];
-  rating_top: number;
-  rating: number;
-  description_raw: string;
-}
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,12 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import genres from "../data/genre";
 import APIClients from "../services/api-clients";
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
-
+import { Genre } from "../entities/Genre";
 const apiClient = new APIClients<Genre>("/genres");
 
 const useGenres = () =>
